38b4dd7827d550b2c91a446eb009c32b1e761014
alemoralespereira <analista.amorales@gmail.com>
2025-06-12 16:43:51 -0300

    solucionando bug

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = URL.create(
-    drivername="postgresql+psycopg2",
-    username="postgres",
-    password="Contabia2024Secure!",
-    host="db.poovgzyvfwrcbqgqvaqk.supabase.co",
-    port=5432,
-    database="postgres"
-)
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# 🔧 Forzamos conexión por IPv4 agregando `?sslmode=require` directamente
+engine = create_engine(DATABASE_URL + "?sslmode=require", connect_args={"sslmode": "require"})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

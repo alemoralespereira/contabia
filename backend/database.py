2a2e15d5f659b5b0a8b1474176c8930912c1a314
alemoralespereira <analista.amorales@gmail.com>
2025-06-12 16:40:35 -0300

    SQLAlchemy

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL").replace(
-    "db.poovgzyvfwrcbqgqvaqk.supabase.co",
-    "3.231.20.200"
-) + "?sslmode=require"
+DATABASE_URL = URL.create(
+    drivername="postgresql+psycopg2",
+    username="postgres",
+    password="Contabia2024Secure!",
+    host="db.poovgzyvfwrcbqgqvaqk.supabase.co",
+    port=5432,
+    database="postgres"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
 Base = declarative_base()

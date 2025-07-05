45ee6aa2ad3d84ed793c983239716f941520ce28
alemoralespereira <analista.amorales@gmail.com>
2025-07-05 01:40:39 -0300

    problemas con styles.css

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/Login.css';
+import '../styles.css';
 
 const Login = ({ onLogin }) => {
   const [formData, setFormData] = useState({ email: '', password: '' });
